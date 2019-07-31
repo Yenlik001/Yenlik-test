1d0090722ea67806eb8a89080e80979a5af164f9
Yenlik_testing <enlik001@gmail.com>
2019-07-31 12:11:30 +0600

    finish roman class

--- src/test/java/tudelft/roman/integertoroman.java
+++ src/test/java/tudelft/roman/integertoroman.java
@@ -4,11 +4,10 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class integertoroman {
-    private static int[] bases = { 9, 7, 5, 3, 1 };
+    private static int[] bases = {9, 7, 5, 3, 1};
     private static HashMap<Integer, String> map = new HashMap<Integer, String>();
 
-    private static void setup()
-    {
+    private static void setup() {
         map.put(1, "I");
         map.put(3, "III");
         map.put(5, "V");
@@ -16,14 +15,11 @@ public class integertoroman {
         map.put(9, "IX");
     }
 
-    public String intToRoman(int num)
-    {
+    public String intToRoman(int num) {
         setup();
         String result = new String();
-        for (int i : bases)
-        {
-            while (num >= i)
-            {
+        for (int i : bases) {
+            while (num >= i) {
                 result += map.get(i);
                 num -= i;
             }
@@ -31,14 +27,15 @@ public class integertoroman {
         return result;
     }
 
-    public static void main(String arg[])
-    {
+    public static void main(String arg[]) {
         System.out.println("Enter the number : ");
         Scanner sc = new Scanner(System.in);
         int no = sc.nextInt();
         integertoroman in = new integertoroman();
-        int value=no;
+        int value = no;
         String sd = in.intToRoman(value);
-        System.out.println(value+" ---> " + sd);
+        System.out.println(value + " ---> " + sd);
     }
 }
+
+
